692bba62c0549f38921e788ade5034b7fb01d5cd
vnay-dev <cleancode4dev@gmail.com>
2022-03-13 23:27:45 +0530

    cookies and session storage

--- local storage/one.js
+++ local storage/one.js
@@ -1,20 +1,26 @@
 // localStorage.setItem("key1","val1") // set data
 // localStorage.getItem("key1")        // val1
-// localStorage.setItem("key1","val2") // overwrites 
+// localStorage.setItem("key1","val2") // overwrites
 // localStorage.removeItem("key1")     // removes key1 data pair
 // localStorage.clear()                // clears everything
 
 // following is a mistake
 let user = {
-    name : "Elon"
-}
-localStorage.setItem("user", user)  // this stores the value pair as [object Object]
+  name: "Elon",
+};
+localStorage.setItem("user", user); // this stores the value pair as [object Object]
 // in the above code the function tries to parse the object into string
 // like String(user) will be "[object Object]"
-localStorage.getItem("user")  // gives us "[object Objet]"
+localStorage.getItem("user"); // gives us "[object Objet]"
 
-// correct way 
-localStorage.setItem("user", JSON.stringify(user))  // this stores the object as { name : "Elon" }
-JSON.parse(localStorage.getItem("user"))            // gives us the object 
+// correct way
+localStorage.setItem("user", JSON.stringify(user)); // this stores the object as { name : "Elon" }
+JSON.parse(localStorage.getItem("user")); // gives us the object
 
+// session storage methods are written exactly the same way :
+sessionStorage.setItem("user", user);
 
+// cookies
+document.cookie = "name=Bob";
+document.cookie = "name=Bob; expires=" + new Date(2020,0,1).toUTCString() // jan 1st
+console.log(document.cookie)    // is the only way to display cookie
